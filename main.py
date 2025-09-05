d2db97b8f339ee7c798a0126a87792eba1f8f70c
Complicated0x <danil.merkulov.09@gmail.com>
2025-09-05 13:18:21 +0400

    refactor and debug

--- main.py
+++ main.py
@@ -1,97 +1,110 @@
 import asyncio
 import struct
 import datetime
+import logging
+import json
 
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 8001
+SERVER_HOST = 'localhost'
+SERVER_PORT = 9000
 
-def crc8_calc (buffer: bytearray):
-    crc8_table = [
-        0x00, 0x31, 0x62, 0x53, 0xC4, 0xF5, 0xA6, 0x97, 0xB9, 0x88, 0xDB, 0xEA, 0x7D, 0x4C, 0x1F, 0x2E,
-        0x43, 0x72, 0x21, 0x10, 0x87, 0xB6, 0xE5, 0xD4, 0xFA, 0xCB, 0x98, 0xA9, 0x3E, 0x0F, 0x5C, 0x6D,
-        0x86, 0xB7, 0xE4, 0xD5, 0x42, 0x73, 0x20, 0x11, 0x3F, 0x0E, 0x5D, 0x6C, 0xFB, 0xCA, 0x99, 0xA8,
-        0xC5, 0xF4, 0xA7, 0x96, 0x01, 0x30, 0x63, 0x52, 0x7C, 0x4D, 0x1E, 0x2F, 0xB8, 0x89, 0xDA, 0xEB,
-        0x3D, 0x0C, 0x5F, 0x6E, 0xF9, 0xC8, 0x9B, 0xAA, 0x84, 0xB5, 0xE6, 0xD7, 0x40, 0x71, 0x22, 0x13,
-        0x7E, 0x4F, 0x1C, 0x2D, 0xBA, 0x8B, 0xD8, 0xE9, 0xC7, 0xF6, 0xA5, 0x94, 0x03, 0x32, 0x61, 0x50,
-        0xBB, 0x8A, 0xD9, 0xE8, 0x7F, 0x4E, 0x1D, 0x2C, 0x02, 0x33, 0x60, 0x51, 0xC6, 0xF7, 0xA4, 0x95,
-        0xF8, 0xC9, 0x9A, 0xAB, 0x3C, 0x0D, 0x5E, 0x6F, 0x41, 0x70, 0x23, 0x12, 0x85, 0xB4, 0xE7, 0xD6,
-        0x7A, 0x4B, 0x18, 0x29, 0xBE, 0x8F, 0xDC, 0xED, 0xC3, 0xF2, 0xA1, 0x90, 0x07, 0x36, 0x65, 0x54,
-        0x39, 0x08, 0x5B, 0x6A, 0xFD, 0xCC, 0x9F, 0xAE, 0x80, 0xB1, 0xE2, 0xD3, 0x44, 0x75, 0x26, 0x17,
-        0xFC, 0xCD, 0x9E, 0xAF, 0x38, 0x09, 0x5A, 0x6B, 0x45, 0x74, 0x27, 0x16, 0x81, 0xB0, 0xE3, 0xD2,
-        0xBF, 0x8E, 0xDD, 0xEC, 0x7B, 0x4A, 0x19, 0x28, 0x06, 0x37, 0x64, 0x55, 0xC2, 0xF3, 0xA0, 0x91,
-        0x47, 0x76, 0x25, 0x14, 0x83, 0xB2, 0xE1, 0xD0, 0xFE, 0xCF, 0x9C, 0xAD, 0x3A, 0x0B, 0x58, 0x69,
-        0x04, 0x35, 0x66, 0x57, 0xC0, 0xF1, 0xA2, 0x93, 0xBD, 0x8C, 0xDF, 0xEE, 0x79, 0x48, 0x1B, 0x2A,
-        0xC1, 0xF0, 0xA3, 0x92, 0x05, 0x34, 0x67, 0x56, 0x78, 0x49, 0x1A, 0x2B, 0xBC, 0x8D, 0xDE, 0xEF,
-        0x82, 0xB3, 0xE0, 0xD1, 0x46, 0x77, 0x24, 0x15, 0x3B, 0x0A, 0x59, 0x68, 0xFF, 0xCE, 0x9D, 0xAC
-]
-    crc = 0xFF
-    for byte in buffer:
-        crc = crc8_table[crc ^ byte]
-    return crc
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger(__name__)
 
-def xor_sum(buffer):
+def xor_sum(buffer) -> bytes:
     temp_sum = 0
     for byte in buffer:
         temp_sum ^= byte
     return temp_sum
 
 def make_handshake(receiverId: int, senderId: int, data: bytes) -> bytes:
-    header = bytearray(b"@NTC")
-    
-    header += senderId
-    header += receiverId
-    header += len(data).to_bytes(2, "little")
-    header.append(xor_sum(data))
-    header.append(0)
-    header[15] = xor_sum(header[:15])
-    return bytes(header) + data
+    """Authorization"""
+    try:
+        header = bytearray(b"@NTC")
+            
+        header += senderId
+        header += receiverId
+        
+        if not isinstance(data, bytes):
+            raise TypeError(f"Data must be bytes, got {type(data)}")
+        
+        size = len(data).to_bytes(2, "little")
+        header += size
+        
+        try:
+            checksum_data = xor_sum(data)
+            header.append(checksum_data)
+        except Exception as e:
+            raise RuntimeError(f"Failed to calculate data checksum: {e}")
+        
+        header.append(0)
+        try:
+            checksum_header = xor_sum(header[:15])
+            header[15] = checksum_header
+        except Exception as e:
+            raise RuntimeError(f"Failed to calculate header checksum {e}")
+            
+        return bytes(header) + data
+    except Exception as e:
+        logger.error(f"Handshake creation failed: {e}")
 
 async def handle(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    data = await reader.read(100)
-    
-    idobj = data[4:8]
-    iddc = data[8:12]
-    
-    device_id = data.decode('ascii').split("S:")[1]
+    """Server handler"""
+    client = writer.get_extra_info('peername')
+    logger.info(f"Новое подключение к серверу: {client}")
+    try:
+        data = await reader.read(100)
+        if not data:
+            logger.warning(f"Client {client} has disconnected")
+            return
+        idobj = data[4:8]
+        iddc = data[8:12]
+        device_id = data.decode('ascii').split("S:")[1]
+    except asyncio.IncompleteReadError:
+        logger.warning(f"Client {client} terminated the connection")
     
     response = make_handshake(idobj, iddc, data=b"*<S")
-    
     writer.write(response)
-    print(device_id)
+    logger.info("Handshake Good",)
+    
     await writer.drain()
     try:
         while True:
-            more_data = await reader.read(100)
-            if not more_data:
-                print("No data more")
+            try:
+                more_data = await reader.read(100)
+                if not more_data:
+                    break
+                unpacked_timestamp = struct.unpack('<I', more_data[8:12])[0]
+                timestamp = datetime.datetime.fromtimestamp(unpacked_timestamp)
+                # timestamp_last = struct.unpack('<I', more_data[16:20])[0]
+                # dt_last = datetime.datetime.fromtimestamp(timestamp_last)
+                
+                latitude = struct.unpack('<I', more_data[20:24])[0]
+                longitude = struct.unpack('<I', more_data[24:28])[0]
+                speed = struct.unpack('<I', more_data[28:32])[0]
+            except struct.error:
+                logger.error(f"Client {client} sent broken data")
                 break
-            print(more_data)
-            timestamp_bytes = more_data[8:12]
-            timestamp = struct.unpack('<I', timestamp_bytes)[0]
-            dt = datetime.datetime.fromtimestamp(timestamp)
-            timestamp_bytes_last = more_data[16:20]
-            timestamp_last = struct.unpack('<I', timestamp_bytes_last)[0]
-            dt_last = datetime.datetime.fromtimestamp(timestamp_last)
-            
-            latitude_bytes = more_data[20:24]
-            latitude = struct.unpack('<I', latitude_bytes)[0]
-            longitude_bytes = more_data[24:28]
-            longitude = struct.unpack('<I', longitude_bytes)[0]
-            speed_bytes = more_data[28:32]
-            speed = struct.unpack('<I', speed_bytes)[0]
-            # print(f"\nMore data: {more_data.hex()}")
-            print(f"===TIMESTAMP===\n{dt}\n======TIMESTAMP LAST====={dt_last}")
-            print(latitude/1000000)
-            print(longitude/1000000)
-            print(speed)
-            
+            if datetime.datetime.now().year == timestamp.year:
+                logger.info(pack_data(device_id, str(timestamp), latitude, longitude, speed))
     except Exception as e:
-        print(f"Ошибка {e}")
+        logger.error(f"Error {e}")
     
-    
+def pack_data(device_id: int, timestamp: str, latitude: int, longitude: int, speed: int) -> str:
+    data = {
+        "device_id": device_id,
+        "timestamp": timestamp,
+        "latitude": latitude / 1000000,
+        "longitude": longitude / 1000000,
+        "speed": speed
+        }
+    return json.dumps(data, indent=4)
+        
 async def main():
-    server = await asyncio.start_server(handle, SERVER_HOST, SERVER_PORT)
-    async with server:
-        await server.serve_forever()
+    try:
+        server = await asyncio.start_server(handle, SERVER_HOST, SERVER_PORT)
+        async with server:
+            await server.serve_forever()
+    except OSError as e:
+        logger.error(f"Network or port error: {e}")
 
 asyncio.run(main())
